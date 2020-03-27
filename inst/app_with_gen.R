d205896f7351cf6ed516b6025ecf1ddf5dfcf98c
Devin Pastoor <devinp@metrumrg.com>
2020-03-26 22:47:55 -0400

    x: exploring diff module types

--- inst/app_with_gen.R
+++ inst/app_with_gen.R
@@ -1,20 +1,20 @@
 library(shiny)
-.mt <- yaml::read_yaml("example_gen.yaml")
-.physio <- yaml::read_yaml("physio.yaml")
-generate_module(.mt) %>% write_module("pkModule.R")
-generate_module(.physio) %>% write_module("physioModule.R")
+# .mt <- yaml::read_yaml("example_gen.yaml")
+#  .multi <- yaml::read_yaml("multi-input.yml")
+# # generate_module(.mt) %>% write_module("pkModule.R")
+#  generate_module(.multi) %>% write_module("multiModule.R")
 source("pkModule.R")
-source("physioModule.R")
+source("multiModule.R")
 ui <- fluidPage(
   shinyFeedback::useShinyFeedback(),
   pkUI("pk"),
-  physioUI("physio"),
+  multiUI("multi"),
   verbatimTextOutput("value")
 )
 
 server <- function(input, output) {
   pkVals <- pkServer("pk")
-  physioVals <- physioServer("physio")
+  multiVals <- multiServer("multi")
   output$value <- renderText({ sprintf("vka: %s, vFa: %s, vCL_Ki: %s",
                                        pkVals$vka(), pkVals$vFa(), pkVals$vCL_Ki()) })
 }
